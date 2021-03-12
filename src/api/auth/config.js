d8c3fa25644e97a34e71ecfdaba01cb8e7178fe4
Daniel <danielkarpen97@gmail.com>
2021-03-11 18:16:09 -0600

    api with firebase auth

---
+++ src/api/auth/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyC5EUB4L04k6o2ZmLC691kneRx89TAY6mI',
+  authDomain: 'login-registration-6ff9b.firebaseapp.com',
+  projectId: 'login-registration-6ff9b',
+  storageBucket: 'login-registration-6ff9b.appspot.com',
+  messagingSenderId: '719419176640',
+  appId: '1:719419176640:web:63a50a21bd79be652a1f25',
+};
+
+export default config;
---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import auth from './auth';
+
+const api = {
+  show(email, password) {
+    console.log(auth);
+  },
+  create(email, password) {},
+  update(email) {},
+};
+
+export default api;
